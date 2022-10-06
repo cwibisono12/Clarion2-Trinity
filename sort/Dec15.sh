67e83e4cc368464cd96730668c1df6404fdd1616
cw19s <cw19s@fsu.edu>
2022-10-06 14:41:44 -0400

    example of how to run runall.sh

---
+++ sort/Dec15.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#===================================================================================================================
+#Program to run the converter
+#v1.0 Jan/14/2022 C.W
+#How to run the program
+
+#Example:
+#./pol_auto.sh 2861 2890 ev2 ev2_pol_78 sf/pol/DopplerCorrection/00128/calibration_R2990.sf pair.pol
+#====================================================================================================================
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-01 15 01 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-02 15 02 2d_all_p.banx 
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-03 15 03 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-04 15 04 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-05 15 05 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-06 15 06 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-07 15 07 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-08 15 08 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-09 15 09 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-10 15 10 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-11 15 11 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-12 15 12 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-13 15 13 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-14 15 14 2d_all_p.banx
+./runall.sh 0 9 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-15 15 15 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-01 15 01 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-02 15 02 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-03 15 03 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-04 15 04 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-05 15 05 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-06 15 06 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-07 15 07 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-08 15 08 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-09 15 09 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-10 15 10 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-11 15 11 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-12 15 12 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-13 15 13 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-14 15 14 2d_all_p.banx
+./runall.sh 10 99 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-15 15 15 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-01 15 01 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-02 15 02 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-03 15 03 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-04 15 04 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-05 15 05 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-06 15 06 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-07 15 07 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-08 15 08 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-09 15 09 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-10 15 10 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-11 15 11 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-12 15 12 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-13 15 13 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-14 15 14 2d_all_p.banx
+./runall.sh 100 200 /mnt/data0/2021_12_O16_gammapn_cw19s/fsu_run_2021/Dec15_TO/Dec15-15 15 15 2d_all_p.banx
