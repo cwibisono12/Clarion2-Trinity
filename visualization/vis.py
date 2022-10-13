799ecd3f1ad7be4038172196daaa24b9d5807faa
cw19s <cw19s@fsu.edu>
2022-10-13 03:01:34 -0400

    revise visualization program for enabling the zooming on projected axis

--- visualization/vis.py
+++ visualization/vis.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#./2Dcut.py file 5000 5000 0 4999 0 4999 
+#./2Dvis.py file 5000 5000 0 4999 0 4999 
 #C. Wibisono
 
 
@@ -16,9 +16,10 @@ from matplotlib.colors import LogNorm
 
 print("2D visualization with the projection\n")
 print("Hit g to make Projection")
-print("Hit e to expand axis (currently only work for zooming figure1")
-print("Hit f to generate coordinates useful formaking banana gates")
+print("Hit e to expand axis")
+print("Hit f to generate coordinates useful for making banana gates")
 print("click mouse to draw banana gates")
+print("Hit q to close figure")
 
 #Reading Matrix Files:
 y=np.fromfile(sys.argv[1],dtype=np.int32,sep="",count=-1)
@@ -36,10 +37,12 @@ plt.colorbar(pos,ax=ax)
 
 #1D-Matrix:
 figp, axp=plt.subplots()
-
+#axp=[]
 #Prepared the array for y and x:
 yproj=np.zeros(int(sys.argv[2]),dtype=np.int32) #Project onto Y axis
 xproj=np.zeros(int(sys.argv[3]),dtype=np.int32) #Project onto X axis
+xx=np.zeros(int(sys.argv[3]),dtype=np.int32)
+xy=np.zeros(int(sys.argv[2]),dtype=np.int32)
 #x=np.arange(0,int(sys.argv[2]),1)
 #x=np.zeros(100)
 #y=np.zeros(100)
@@ -53,6 +56,8 @@ coordsx=[]
 coordsy=[]
 
 index=0
+proj=0
+
 def onclick(event):
 	global index
 	index=index+1
@@ -70,6 +75,7 @@ def onpress(event):
 	if event.key == 'e':
 		print("Enter Axes to Expand 1 or 2\n")
 		waxis=input()
+		global axp,xx,xy,proj
 		if int(waxis) == 1:
 			print("Enter the lower xlim\n")
 			xlow=input()
@@ -86,11 +92,24 @@ def onpress(event):
 			xlow=input()
 			print("Enter the upper limit\n")
 			xup=input()
+			axp.clear()
 			axp.set_xlim(int(xlow),int(xup))
-	
+			if int(proj)==1:	
+				axp.plot(xx[int(xlow):int(xup)],xproj[int(xlow):int(xup)],linewidth=0.75,ls='steps',label='ProjX')
+				axp.legend()
+			if int(proj)==2:
+				axp.plot(xy[int(xlow):int(xup)],yproj[int(xlow):int(xup)],linewidth=0.75,ls='steps',label='ProjY')
+				axp.legend()
+			#axp.autoscale()
+			plt.draw()
 	#Zoom out Histogram:
 	if event.key == 'o':
 		ax.set_xlim(0,int(sys.argv[3]))
+		ax.set_ylim(0,int(sys.argv[2]))
+		if int(proj)==1:
+			axp.set_xlim(0,int(sys.argv[3]))
+		if int(proj)==2:
+			axp.set_xlim(0,int(sys.argv[2]))
 	
 	#Saving Coordinates File for Banana Gates:
 	if event.key == 'f':
@@ -113,22 +132,27 @@ def onpress(event):
 		low=input()
 		print("Enter the higher region:\n")
 		high=input()
-
+		#global axp
 		#Projection on X axis: Make a gate on Y axis then project on X axis:
+		axp.clear()
 		if int(proj) == 1:
-			x=np.arange(0,int(sys.argv[3]),1)
+			#global x
+			xx=np.arange(0,int(sys.argv[3]),1)
 			for i in range(0,int(sys.argv[2]),1):
 				for j in range(int(low),int(high)+1,1):
 					xproj[i]=xproj[i]+ytrp[j,i]	
-			axp.plot(x,xproj,linewidth=0.75,ls='steps')
-
+			axp.plot(xx,xproj,linewidth=0.75,ls='steps',label='ProjX')
+			axp.legend()
 		#Projection on Y axis: Make a gate on X axis then project on Y axis:
 		if int(proj) == 2:
-			x=np.arange(0,int(sys.argv[2]),1)
+			#global x
+			xy=np.arange(0,int(sys.argv[2]),1)
 			for i in range(int(low),int(high)+1,1):
 				for j in range(0,int(sys.argv[2]),1):
 					yproj[i]=yproj[i]+ytrp[i,j]
-			axp.plot(x,yproj,linewidth=0.75,ls='steps')
+			axp.plot(xy,yproj,linewidth=0.75,ls='steps',label='ProjY')
+			axp.legend()
+	'''
 	if event.key == 'n':
 		plt.figure(2)
 		print("Enter the region to expand\n")
@@ -137,8 +161,9 @@ def onpress(event):
 		print("Enter the higher region\n")
 		xupp=input()
 		axp.set_xlim(int(xlowp),int(xupp))
-		axp.autoscale_view()
+		#axp.autoscale()
 		plt.draw()
+	'''
 fig.canvas.mpl_connect('button_press_event',onclick) 
 fig.canvas.mpl_connect('key_press_event',onpress)
 figp.canvas.mpl_connect('button_press_event',onclick)
